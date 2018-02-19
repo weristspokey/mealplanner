d6d937134b0ea5bc87ca406d94eaa2e9e11c898a
weristspokey <4buczko@informatik.uni-hamburg.de>
2018-02-19 18:14:35 +0100

    Add disabling functionality to mealplan item form #30

--- public/js/recipes.js
+++ public/js/recipes.js
@@ -9,6 +9,38 @@ $(document).ready(function(){
     $(".alert").delay(4000).slideUp(2000, function(){
         $(this).alert('close');
     });
+
+
+    disableRecipeSelect();
+
+    $(".btn-group.food-select").find('.btn').click(function() {
+        enableFoodSelect();
+        disableRecipeSelect();
+    });
+    $(".btn-group.recipe-select").find('.btn').click(function() {
+        enableRecipeSelect();
+        disableFoodSelect();
+    });
 });
 
 
+function disableFoodSelect() {
+    $(".btn-group.food-select").addClass('disabled');
+    $(".btn-group.food-select").find('.btn').addClass('disabled');
+    $(".selectpicker.food-select").attr('disabled', 'true');
+}
+function disableRecipeSelect() {
+    $(".btn-group.recipe-select").addClass('disabled');
+    $(".btn-group.recipe-select").find('.btn').addClass('disabled');
+    $(".selectpicker.recipe-select").attr('disabled', 'true');
+}
+function enableFoodSelect() {
+    $(".btn-group.food-select").removeClass('disabled');
+    $(".btn-group.food-select").find('.btn').removeClass('disabled');
+    $(".selectpicker.food-select").removeAttr('disabled');
+}
+function enableRecipeSelect() {
+    $(".btn-group.recipe-select").removeClass('disabled');
+    $(".btn-group.recipe-select").find('.btn').removeClass('disabled');
+    $(".selectpicker.recipe-select").removeAttr('disabled');
+}
