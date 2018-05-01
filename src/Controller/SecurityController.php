ff5720e3cf1e0d7731407db2c143e722f0b61627
weristspokey <4buczko@informatik.uni-hamburg.de>
2018-05-02 00:02:25 +0200

    New registration

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\LoginType;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends Controller
 {
@@ -32,6 +33,39 @@ class SecurityController extends Controller
     public function logoutAction()
     {
         throw new \RuntimeException("Is never been called");
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $registerForm = $this->createForm(UserType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $registerForm->handleRequest($request);
+        if ($registerForm->isSubmitted() && $registerForm->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+            $this->addFlash('success', 'You are registered');
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render(
+            'register.html.twig',
+            array('form' => $registerForm->createView())
+        );
     }
 }
 ?>
